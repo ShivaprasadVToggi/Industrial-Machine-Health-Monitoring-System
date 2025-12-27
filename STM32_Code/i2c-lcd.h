7aee9c619d6f9bd660289da028d4a33a4acbaa35
Shivaprasad V Toggi <shivaprasadtoggi@gmail.com>
2025-12-27 10:33:32 +0530

    Create i2c-lcd.h

---
+++ STM32_Code/i2c-lcd.h
@@ -0,0 +1,12 @@
+#ifndef INC_I2C_LCD_H_
+#define INC_I2C_LCD_H_
+
+#include "stm32f4xx_hal.h"
+
+void lcd_init (void);
+void lcd_send_cmd (char cmd);
+void lcd_send_data (char data);
+void lcd_send_string (char *str);
+void lcd_clear (void);
+
+#endif /* INC_I2C_LCD_H_ */
